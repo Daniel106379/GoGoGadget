a50d93567d5e40e0f4f2355b17b3a03158ab82c6
Daniel106379 <danspad31@gmail.com>
2025-03-04 13:17:28 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$get = $_GET['get'];
+$mpdUrl = $get;
+
+$mpdheads = [
+  'http' => [
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
+];
+$context = stream_context_create($mpdheads);
+$res = file_get_contents($mpdUrl, false, $context);
+echo $res;
+?>
